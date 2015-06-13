a313d664eee5d9991c63b96987520f6889f47589
mryoshio <yoshiokaas@gmail.com>
2015-06-14 01:53:08 +0900

    initial fix on test-kitchen for local test

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures sample_cookbook'
 long_description 'Installs/Configures sample_cookbook'
 version          '0.1.0'
+
+depends 'nginx'
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,10 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+case node[:platform]
+when 'debian', 'ubuntu'
+  include_recipe 'apt'
+end
+
+include_recipe 'nginx'
